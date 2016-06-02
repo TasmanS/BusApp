ef2fe98fa9c2ddd686eb6fed818ef8c0031593af
Troy Sweetland <Troysweetland@gmail.com>
2016-06-02 12:17:32 -0700

    Auto Refresh, Needs Threading, Adds new lecture

--- Bus App.py
+++ Bus App.py
@@ -1,18 +1,21 @@
 import urllib.request
 import xml.etree.ElementTree as ET
 import tkinter as tk
-import datetime
+import time
+
 
 class busSchedule():
     def __init__(self):
-        self.schedule=["--","--","--"]
+        self.schedule=[]
 
     def checkRoute(self):
+
+        self.schedule=[]
        
         z=urllib.request.urlopen("http://services.my511.org/Transit2.0/GetNextDeparturesByStopCode.aspx?token=1c4f133b-4036-4b61-a5ce-e3b24b651047&stopcode=14443")
         try: data=z.read()
         except:
-            return ["--","--","--"]
+            return ["-","-","-"]
         
         f=open("rt22.xml", "wb")
         f.write(data)
@@ -21,15 +24,14 @@ class busSchedule():
         doc=ET.parse("rt22.xml")
         stopschedule=doc.getroot()
 
-        schedule=[]
         for entry in stopschedule.iter("DepartureTime"):
-            schedule+=[entry.text]
+            self.schedule+=[entry.text]
             
-        while len(schedule)<3:
-            schedule+="--"
+        while len(self.schedule)<3:
+            self.schedule+=["-"]
         
-        print(schedule)
-        return(schedule)
+        print(self.schedule)
+        return(self.schedule)
 
 
 
@@ -41,23 +43,35 @@ class Application():
         self.w.create_rectangle(555,15,975,240, fill="dim gray", width=10)
         self.w.create_rectangle(555,260,975,485, fill="dim gray", width=10)
 
+
     def popItems(self,schedule):
-        self.w.create_text(270,250, fill="white", font=("helvetica",250),
+        self.t1=self.w.create_text(275,250, fill="white", font=("helvetica",250),
                            justify="center", text=schedule[0])
-        self.w.create_text(765,130, fill="white", font=("helvetica",125),
+        self.t2=self.w.create_text(770,130, fill="white", font=("helvetica",125),
                            justify="center", text=schedule[1])     
-        self.w.create_text(765,375, fill="white", font=("helvetica",125),
+        self.t3=self.w.create_text(770,375, fill="white", font=("helvetica",125),
                            justify="center", text=schedule[2])
 
 
-## set how often this updates - likely 30 seconds?
-    def updateItems(self,schedule):
-        pass
+    def updateItems(self,busobject):
+        latestRoute=busobject.checkRoute()
+        self.w.itemconfig(self.t1,text=latestRoute[0])
+        self.w.itemconfig(self.t2,text=latestRoute[1])
+        self.w.itemconfig(self.t3,text=latestRoute[2])
+        self.w.update()
+        time.sleep(30)
+        self.updateItems(busobject)
+        
 
 
 fortyEight=busSchedule()
 app = Application()
 app.popItems(fortyEight.checkRoute())
+app.updateItems(fortyEight)
+
+
+
+
 
 
 
